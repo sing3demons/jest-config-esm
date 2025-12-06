3d1963d9197e8f26f66159446caaba41d0a97305
sing <sing3demons@live.com>
2025-12-06 11:30:10 +0700

    Add Jest configuration for TypeScript and ESM
    Configure Jest for TypeScript with ESM support.

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import("jest").Config} **/
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          verbatimModuleSyntax: false,
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
